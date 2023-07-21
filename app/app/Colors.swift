0dcca56b293ae3d4af162c6a68cd642f5343f208
JD <jann.driessen@gmx.de>
2023-07-21 20:13:16 +0200

    add empty colors and config

---
+++ app/app/Colors.swift
@@ -0,0 +1,8 @@
+//
+//  Colors.swift
+//  app
+//
+//  Created by Jann Driessen on 21.07.23.
+//
+
+import Foundation
---
+++ app/app/Config.swift
@@ -0,0 +1,8 @@
+//
+//  Config.swift
+//  app
+//
+//  Created by Jann Driessen on 21.07.23.
+//
+
+import Foundation
