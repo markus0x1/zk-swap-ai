b2b26f31abd3180b221e283d32f0772ffe02b3a3
JD <jann.driessen@gmx.de>
2023-07-21 21:19:57 +0200

    add empty classes for app

--- app/app/appApp.swift
+++ app/app/appApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct appApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                ConnectView()
+            }
         }
     }
 }
---
+++ app/app/ui/ConfirmationScreen.swift
@@ -0,0 +1,20 @@
+//
+//  ConfirmationScreen.swift
+//  app
+//
+//  Created by Jann Driessen on 21.07.23.
+//
+
+import SwiftUI
+
+struct ConfirmationScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ConfirmationScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        ConfirmationScreen()
+    }
+}
