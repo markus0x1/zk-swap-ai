35abe70d3a06c65a10bfcfe3e66da4031cb5bfda
JD <jann.driessen@gmx.de>
2023-07-21 20:13:24 +0200

    add basic colors setup

--- app/app/Colors.swift
+++ app/app/Colors.swift
@@ -5,4 +5,41 @@
 //  Created by Jann Driessen on 21.07.23.
 //
 
-import Foundation
+import SwiftUI
+
+enum SwapAiColor {
+    static let black = Color(hex: 0x000000)
+}
+
+// MARK: - Extensions
+
+extension Color {
+    init(hex: UInt, alpha: Double = 1) {
+        self.init(
+            .sRGB,
+            red: Double((hex >> 16) & 0xff) / 255,
+            green: Double((hex >> 08) & 0xff) / 255,
+            blue: Double((hex >> 00) & 0xff) / 255,
+            opacity: alpha
+        )
+    }
+
+    init(light: Color, dark: Color) {
+      self.init(UIColor(light: UIColor(light), dark: UIColor(dark)))
+    }
+}
+
+extension UIColor {
+  convenience init(light: UIColor, dark: UIColor) {
+    self.init { traitCollection in
+      switch traitCollection.userInterfaceStyle {
+      case .light, .unspecified:
+        return light
+      case .dark:
+        return dark
+      @unknown default:
+        return light
+      }
+    }
+  }
+}
