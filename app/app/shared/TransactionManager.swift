2def0776b562b268dad01aadd351848923a111ba
JD <jann.driessen@gmx.de>
2023-07-22 08:15:12 +0200

    integrate managers into ui flow

--- app/app/shared/TransactionManager.swift
+++ app/app/shared/TransactionManager.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+class TransactionManager {
+    private (set) var voiceResponse: VoiceAnalyzerResponse?
+
+    func setVoiceResponse(_ response: VoiceAnalyzerResponse) {
+        self.voiceResponse = response
+    }
+
+    func signAndSend() async throws {
+        print("signAndSend")
+    }
+}
--- app/app/ui/ConfirmationScreen.swift
+++ app/app/ui/ConfirmationScreen.swift
@@ -8,12 +8,23 @@
 import SwiftUI
 
 struct ConfirmationScreen: View {
+    let transactionManager: TransactionManager
     @State private var isPresented = false
     var body: some View {
         VStack {
+            Text("Swap \(txDetails.inputAmount) \(txDetails.inputToken) for \(txDetails.outputToken)")
             Spacer()
             Button("Sign&Send") {
-                isPresented = true
+                Task.init {
+                    do {
+                        try await transactionManager.signAndSend()
+                        // TODO: only forward once tx is done?
+                        // TODO: pass tx information (to display on next screen)?
+                        isPresented = true
+                    } catch {
+                        print("Error sending tx", error)
+                    }
+                }
             }
         }
         .padding()
@@ -21,10 +32,13 @@ struct ConfirmationScreen: View {
             SuccessScreen()
         }
     }
+    private var txDetails: VoiceAnalyzerResponse {
+        return transactionManager.voiceResponse ?? VoiceAnalyzerResponse(inputToken: "", outputToken: "", inputAmount: 0)
+    }
 }
 
 struct ConfirmationScreen_Previews: PreviewProvider {
     static var previews: some View {
-        ConfirmationScreen()
+        ConfirmationScreen(transactionManager: TransactionManager())
     }
 }
