c0ea4f73cc02289ccf3f57973f45a90e25a61616
ümit.eth <47325422+UemitGuel@users.noreply.github.com>
2023-07-22 20:16:51 +0200

    add

--- app/app/ui/RecordScreen.swift
+++ app/app/ui/RecordScreen.swift
@@ -5,34 +5,57 @@
 //  Created by Jann Driessen on 21.07.23.
 //
 
+import Foundation
+import Foundation
+import AVFoundation
+import Speech
 import SwiftUI
+
+extension AVPlayer {
+    static let sharedDingPlayer: AVPlayer = {
+        guard let url = Bundle.main.url(forResource: "ding", withExtension: "wav") else { fatalError("Failed to find sound file.") }
+        return AVPlayer(url: url)
+    }()
+}
 
 struct RecordScreen: View {
     @ObservedObject private var connectionManager: ConnectionManager
     private let transactionManager: TransactionManager
-    @State private var isTalking = false
     @State private var isPresented = false
+    
+    @StateObject var speechRecognizer = SpeechRecognizer()
+    @State private var isTalking = false
+    
+    @State var transcript = "test"
+    
+    private var player: AVPlayer { AVPlayer.sharedDingPlayer }
+
     init(connectionManager: ConnectionManager) {
         self.connectionManager = connectionManager
         self.transactionManager = TransactionManager(connectionManager: connectionManager)
     }
     var body: some View {
         VStack {
+            Text(transcript)
             GlowingButton(isTalking: $isTalking)
                 .onTapGesture {
                     Task.init {
+                        speechRecognizer.resetTranscript()
+                        speechRecognizer.startTranscribing()
                         isTalking = true
                         // wait for 1 sec to simulate listening
-                        try await Task.sleep(nanoseconds: 1_000_000_000)
+                        try await Task.sleep(nanoseconds: 10_000_000_000)
+                        speechRecognizer.stopTranscribing()
+                        transcript = speechRecognizer.transcript
                         let openAI = OpenAIService()
                         let analyzer = VoiceAnalyzer(openAIService: openAI)
                         // FIXME: note some of the classes only return dummy data for now (change for production)
                         // TODO: use result of voice recording (text)
-                        let res = await analyzer.analyzeText("I want to swap 1 ETH to USDC")
+                        let res = await analyzer.analyzeText(transcript)
                         print("////OPENAIRES")
                         print(res)
                         transactionManager.setVoiceResponse(res!)
-                        isPresented = true
+//                        isPresented = true
                     }
                 }
             Text("Say something like\n'I wanna swap 0.1 ETH for USDC'")
