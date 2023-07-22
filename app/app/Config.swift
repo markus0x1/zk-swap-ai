cc7478ab467e43d38ea9e56eb976a5a370e63f72
JD <jann.driessen@gmx.de>
2023-07-22 17:07:06 +0200

    add constants

--- app/app/Config.swift
+++ app/app/Config.swift
@@ -10,3 +10,11 @@ import Foundation
 enum Config {
     static let apiKeyOpenAI = ""
 }
+
+extension Config {
+    enum Constants {
+        static let safeAddress = "0x1911B66711161807bD6E0078364Bf04Ac60F7b95"
+        static let WETH = "0x1684F4DF5e32a946fBbaEb3059353c83Ff075E31"
+        static let DAI = "0xDAFA240382BE6e8Fb5b13D1516d3d220Cf5A1622"
+    }
+}
