5489a344619a5fba9c242ffb17b8e836dfc7ded5
JD <jann.driessen@gmx.de>
2023-07-22 23:30:39 +0200

    add wallet connect signing

--- app/app/ui/ConfirmationScreen.swift
+++ app/app/ui/ConfirmationScreen.swift
@@ -9,10 +9,12 @@ import Combine
 import Foundation
 import metamask_ios_sdk
 import SwiftUI
+import WalletConnectModal
 
 struct ConfirmationScreen: View {
     let transactionManager: TransactionManager
     @State private var cancellables: Set<AnyCancellable> = []
+    @State private var publishers: Set<AnyCancellable> = []
     @State private var isPresented = false
     var body: some View {
         ZStack() {
@@ -50,7 +52,55 @@ struct ConfirmationScreen: View {
 
 extension ConfirmationScreen {
 
-    private func signWithWalletConnect() {}
+    /// work in progress - not tested
+    private func signWithWalletConnect() {
+        // Create a signing request
+//        let uri = try await Pair.instance.create()
+//        try await Sign.instance.connect(requiredNamespaces: namespaces, topic: uri.topic)
+        Sign.instance.sessionsPublisher
+            .receive(on: DispatchQueue.main)
+            .sink { (sessions: [Session]) in
+                Task.init {
+                    do {
+                        let sendTxRequest = SwapApiSendTxRequest(
+                            safeAddress: Config.Constants.safeAddress,
+                            inToken: Config.Constants.WETH,
+                            outToken: Config.Constants.DAI,
+                            dx: "10000000000000000",
+                            minDy: "0",
+                            nonce: "0",
+                            signature: "0x000000000000000000000000000000000000dead"
+                        )
+                        guard let json = try? JSONEncoder().encode(sendTxRequest) else { return }
+                        let msg = json.toHexString()
+                        // TODO: create request on the "dapp" side to sign data
+                        let chainId = "0x5"
+                        let method = "personal_sign"
+                        // TODO: get connect address
+                        let walletAddress = "0x9b2055d370f73ec7d8a03e965129118dc8f5bf83" // This should match the connected address
+                        let requestParams = AnyCodable([msg, walletAddress])
+                        let request = Request(topic: "session.topic", method: method, params: requestParams, chainId: Blockchain(chainId)!)
+                        try await Sign.instance.request(params: request)
+                    } catch {
+                        print("Error sending signing request for WalletConnect", error)
+                    }
+                }
+            }.store(in: &publishers)
+        // Receive singing proposals
+        Sign.instance.sessionProposalPublisher
+            .receive(on: DispatchQueue.main)
+            .sink { session in
+//                self.verifyDapp(session.context)
+//                self.showSessionProposal(session.proposal)
+            }.store(in: &publishers)
+        // TODO: approve session
+//        Sign.instance.approve(
+//            proposalId: "proposal_id",
+//            namespaces: sessionNamespaces
+//        )
+        // TODO: get signed data and add signature to API request
+        // TODO: call API with assembled data (as in Metamask example below)
+    }
 
     private func signWithMetamask() {
         let ethereum = transactionManager.connectionManager.ethereum
