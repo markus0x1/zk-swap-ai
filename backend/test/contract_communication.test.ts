d510a4af63def9e82adb7bdf307bcec1a9de1125
Stanislaw Baranski <stan@stan.bar>
2023-07-22 18:58:07 +0200

    update get_dy_test

--- backend/test/contract_communication.test.ts
+++ backend/test/contract_communication.test.ts
@@ -3,10 +3,10 @@ import { DAI_ADDRESS, WETH_ADDRESS, getDy } from "../src/ethereum";
 describe.only('proof ethereum calls', () => {
     it('getDy', async () => {
         const dYDai = await getDy("A", 1n, DAI_ADDRESS)
-        console.log(dYDai)
+        expect(dYDai).toBeGreaterThan(0n)
+
         const dYWeth = await getDy("A", 1n, WETH_ADDRESS)
-        console.log(dYWeth)
-        expect(dYWeth).toBeDefined()
+        expect(dYWeth).toBeGreaterThan(0n)
     });
 
 })
